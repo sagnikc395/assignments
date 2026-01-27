f44c0daaa472153d508d77bd2c254a05b4db83fe
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-27 17:03:43 -0500

    feat: middleware using jwt

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const secret = require("../index");
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const token = req.headers.authorization;
+
+  //Token: Bearer <JWT-CODE>
+
+  const words = token.split(" ");
+  const jwtToken = words[1];
+
+  //verify the jwt using the secret
+  const decodedValue = jwt.verify(jwtToken, secret);
+  if (decodedValue.username) {
+    next();
+  } else {
+    res.status(403).json({
+      msg: "you are not authenticated!",
+    });
+  }
 }
 
 module.exports = adminMiddleware;
