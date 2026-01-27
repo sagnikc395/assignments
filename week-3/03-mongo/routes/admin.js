46d35562b57a3330e7dee3375e7c0c0ae84d80ac
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-27 15:43:30 -0500

    feat: add route to signup admin

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,37 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //check if user with this username already exists
+  await Admin.create({
+    username: username,
+    password: password,
+  })
+    .then(function () {
+      res.status(200).json({
+        message: "Admin created successfully!",
+      });
+    })
+    .catch((err) => {
+      res.status(403).json({
+        message: "Could not create Admin!",
+      });
+    });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
 module.exports = router;
