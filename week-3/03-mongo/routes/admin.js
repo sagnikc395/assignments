0855eef53e3a5ffc30de17f7e5c0ecfe0a61dcd8
Sagnik Chatterjee <sagnikchatterjee607@gmail.com>
2026-01-27 15:51:53 -0500

    feat: added the /admin/courses post endpoint

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -26,8 +26,29 @@ router.post("/signup", async (req, res) => {
     });
 });
 
-router.post("/courses", adminMiddleware, (req, res) => {
+router.post("/courses", adminMiddleware, async (req, res) => {
   // Implement course creation logic
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageLink = req.body.imageLink;
+  const price = req.body.price;
+
+  //TODO input validation using Zod
+
+  //add to course -> if key and value are same, then can just use key name
+  const obj = await Course.create({
+    title,
+    description,
+    imageLink,
+    price,
+  });
+
+  const courseId = obj._id;
+
+  res.json({
+    message: "Course created successfully",
+    courseId: courseId,
+  });
 });
 
 router.get("/courses", adminMiddleware, (req, res) => {
